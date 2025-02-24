8800374d806ac9984b0cf6ffd6cbe5cd7be68c0f
Robert James <robert.james.19@ucl.ac.uk>
2025-02-24 22:15:17 +1100

    Update to 10/100 neutron definitions.

--- analyses/CEvNS_discovery/run_analysis_defn1.sh
+++ analyses/CEvNS_discovery/run_analysis_defn1.sh
@@ -10,10 +10,16 @@ for exposure in 100 150 200 250 300 350 400 450 500; do
     python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_LNGS_good_low.pkl -e ${exposure} -c inference_configs/disco_study_no_neutrons.ini -o outputs/CEvNS_disco/LNGS_good_low/60t_${exposure}ty
 done
 
-### Low contaminants plus neutrons
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_LNGS_low_neutrons.ini -t PDFs/pdfs_CEvNS_disco_60t_good.pkl -o CEvNS_disco_60t_1ty_LNGS_good_low_neutrons
+### Low contaminants plus neutrons (low)
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_LNGS_low_neutrons_low.ini -t PDFs/pdfs_CEvNS_disco_60t_good.pkl -o CEvNS_disco_60t_1ty_LNGS_good_low_neutrons_low
 for exposure in 500 600 700 800 900 1000 1100 1200; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_LNGS_good_low_neutrons.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/CEvNS_disco/LNGS_good_low_neutrons/60t_${exposure}ty
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_LNGS_good_low_neutrons_low.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/CEvNS_disco/LNGS_good_low_neutrons_low/60t_${exposure}ty
+done
+
+### Low contaminants plus neutrons (high)
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_LNGS_low_neutrons_high.ini -t PDFs/pdfs_CEvNS_disco_60t_good.pkl -o CEvNS_disco_60t_1ty_LNGS_good_low_neutrons_high
+for exposure in 3000 4000 5000 6000 7000 8000 9000 10000; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_LNGS_good_low_neutrons_high.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/CEvNS_disco/LNGS_good_low_neutrons_high/60t_${exposure}ty
 done
 
 ### High contaminants
@@ -58,10 +64,16 @@ for exposure in 50 100 150 200 250 300 350 400 450; do
     python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_SURF_good_low.pkl -e ${exposure} -c inference_configs/disco_study_no_neutrons.ini -o outputs/CEvNS_disco/SURF_good_low/60t_${exposure}ty
 done
 
-### Low contaminants plus neutrons
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_SURF_low_neutrons.ini -t PDFs/pdfs_CEvNS_disco_60t_good.pkl -o CEvNS_disco_60t_1ty_SURF_good_low_neutrons
+### Low contaminants plus neutrons (low)
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_SURF_low_neutrons_low.ini -t PDFs/pdfs_CEvNS_disco_60t_good.pkl -o CEvNS_disco_60t_1ty_SURF_good_low_neutrons_low
 for exposure in 500 600 700 800 900 1000 1100 1200; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_SURF_good_low_neutrons.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/CEvNS_disco/SURF_good_low_neutrons/60t_${exposure}ty
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_SURF_good_low_neutrons_low.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/CEvNS_disco/SURF_good_low_neutrons_low/60t_${exposure}ty
+done
+
+### Low contaminants plus neutrons (high)
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_SURF_low_neutrons_high.ini -t PDFs/pdfs_CEvNS_disco_60t_good.pkl -o CEvNS_disco_60t_1ty_SURF_good_low_neutrons_high
+for exposure in 3000 4000 5000 6000 7000 8000 9000 10000; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/CEvNS_disco_60t_1ty_SURF_good_low_neutrons_high.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/CEvNS_disco/SURF_good_low_neutrons_high/60t_${exposure}ty
 done
 
 ### High contaminants
