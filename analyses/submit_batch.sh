73d72fa530af59bb6f1b8d77ac12570d4355d26c
Amirr <ms20160@iisermohali.ac.in>
2025-02-27 17:36:45 +0530

    Updated submit_batch.sh

--- analyses/submit_batch.sh
+++ analyses/submit_batch.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
-#################
-#SBATCH -N 1 # number of nodes
-#SBATCH --ntasks-per-node=100 # tasks per node
-#SBATCH -C cpu # hardware architecture
-#SBATCH --time=01:00:00
-#SBATCH --qos=regular
-#SBATCH --job-name=submit_batch
-#SBATCH --license=project,projecta
-#SBATCH -L SCRATCH
-#################
+# Created by the University of Melbourne job script generator for SLURM
+# Wed Jan 29 2025 17:30:51 GMT+0100 (Central European Standard Time)
 
+# Partition for the job:
+#SBATCH --partition=sapphire
 
-#SBATCH --account=lz
-#SBATCH --array=0-0
+# The name of the job:
+#SBATCH --job-name="test_multip"
+
+# The project ID which this job should run under:
+#SBATCH --account="punim1378"
+
+# Maximum number of tasks/CPU cores used by the job:
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=6
+
+# The amount of memory in megabytes per cpu core:
+#SBATCH --mem-per-cpu=25000
+
+# The maximum running time of the job in days-hours:mins:sec
+#SBATCH --time=8-00:0:00
+
+# check that the script is launched with sbatch
+if [ "x$SLURM_JOB_ID" == "x" ]; then
+   echo "You need to submit your job to the queuing system with sbatch"
+   exit 1
+fi
+
+# Run the job from the directory where it was launched (default)
+
+# The modules to load:
+module load Anaconda3/2024.02-1
+
+# The job command(s):
+eval "$(conda shell.bash hook)"
+conda init
+conda activate fd
+
 
 # OpenMP settings:
 export OMP_NUM_THREADS=1
 export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 
-
-module load python
-conda init
-conda activate fire
-
 python3 generate_toys.py -l $1 -m $2 -c $3 -o $4
 
-srun -c 2 --cpu-bind=cores python run_routine.py -l $1 -m $2 -c $3 -o $4
+srun --ntasks=10 -c 6 --cpu-bind=cores python run_routine.py -l $1 -m $2 -c $3 -o $4
+
+
+##DO NOT ADD/EDIT BEYOND THIS LINE##
+##Job monitor command to list the resource usage
+my-job-stats -a -n -s
