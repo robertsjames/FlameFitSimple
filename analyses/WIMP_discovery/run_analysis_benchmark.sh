7a1dec8427939276b1622c4311f2dfa4055ad8d7
Rory Matheson <rory_m@login06.chn.perlmutter.nersc.gov>
2025-06-10 04:30:30 -0700

    Pushing to the remote

--- analyses/WIMP_discovery/run_analysis_benchmark.sh
+++ analyses/WIMP_discovery/run_analysis_benchmark.sh
@@ -1,4 +1,7 @@
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_benchmark.ini -t PDFs/pdfs_WIMP_disco_60t_benchmark.pkl -o WIMP_disco_60t_1ty_benchmark
-for exposure in 250 300 350 400 450 500 550 600 650 700; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/WIMP_disco_60t_1ty_benchmark.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/WIMP_disco/benchmark/60t_${exposure}ty
+
+count=0.1
+
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_benchmark.ini -t PDFs/Pb214_${count}.pkl -o WIMP_disco_60t_1ty_benchmark_${count}
+for exposure in 250 300 350 400 430 440 450 460 470 480 490 500 550 600 650 700; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/WIMP_disco_60t_1ty_benchmark_${count}.pkl -e ${exposure} -c inference_configs/disco_study_neutrons.ini -o outputs/WIMP_disco/benchmark_${count}_run_10/60t_${exposure}ty
 done
