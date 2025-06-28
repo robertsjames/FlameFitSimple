7fa991f8841627fa51ffec7434c2a49e575d937c
Robert James <robert.james.19@ucl.ac.uk>
2025-06-28 11:50:28 -0700

    Add SURF version for discrimination scan.

--- analyses/WIMP_discrimination_scan/run_analysis.sh
+++ analyses/WIMP_discrimination_scan/run_analysis.sh
@@ -9,3 +9,15 @@ for field in 10 15 20 50 100 200 300; do
         fi
     done
 done
+
+## SURF
+for field in 10 15 20 50 100 200 300; do
+    python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_benchmark_SURF.ini -t vFIELD_SCAN_drift_field_${field}.00.pkl -o drift_field_${field}_V_cm_SURF
+    for exposure in 100 300 500 700 1000; do
+        if [[ "$1" == "SLURM" ]]; then
+            sbatch ../submit_batch.sh "python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/drift_field_${field}_V_cm_SURF.pkl -e ${exposure} -c inference_configs/disco_study_SURF.ini -o outputs/SURF/${field}_V_cm/${exposure}_ty"
+        else
+            python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/drift_field_${field}_V_cm_SURF.pkl -e ${exposure} -c inference_configs/disco_study_SURF.ini -o outputs/SURF/${field}_V_cm/${exposure}_ty
+        fi
+    done
+done
