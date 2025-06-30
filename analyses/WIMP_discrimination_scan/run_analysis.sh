6e85fbf2e6614d2294f40b7eb923f76a0a222473
Robert James <robert.james.19@ucl.ac.uk>
2025-06-30 01:51:05 -0700

    Updated stitching script. Updated EFT and ALP sensitivity plots.

--- analyses/WIMP_discrimination_scan/run_analysis.sh
+++ analyses/WIMP_discrimination_scan/run_analysis.sh
@@ -1,3 +1,6 @@
+module load python
+conda activate flamedisx
+
 ## LNGS
 for field in 10 15 20 50 80 100 200 300; do
     python3 ../create_simple_template_likelihood.py -c likelihood_configs/60t_1ty_benchmark_LNGS.ini -t vFIELD_SCAN_drift_field_${field}.00.pkl -o drift_field_${field}_V_cm_LNGS
--- analyses/stitch_all.sh
+++ analyses/stitch_all.sh
@@ -1,10 +1,34 @@
 module load python
 conda activate flamedisx
 
-python3 stitch.py -d SI_WIMP_sensitivity/outputs/SI_WIMP_60t_600ty_LNGS
-
-python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_600ty_LNGS
-
-python3 stitch.py -d ALP_sensitivity/outputs/ALP_60t_600ty
-
 python3 stitch.py -d HP_sensitivity/outputs/HP_60t_600ty
+
+## SI WIMP
+## LNGS
+python3 stitch.py -d SI_WIMP_sensitivity/outputs/SI_WIMP_60t_600ty_LNGS
+## SURF
+python3 stitch.py -d SI_WIMP_sensitivity/outputs/SI_WIMP_60t_600ty_SURF
+
+## O6 (scalar) NREFT WIMP
+## LNGS
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_100ty_LNGS
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_300ty_LNGS
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_600ty_LNGS
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_1000ty_LNGS
+## SURF
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_100ty_SURF
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_300ty_SURF
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_600ty_SURF
+python3 stitch.py -d EFT_O6_s_WIMP_sensitivity/outputs/EFT_O6_s_WIMP_60t_1000ty_SURF
+
+## ALP
+python3 stitch.py -d ALP_sensitivity/outputs/ALP_60t_100ty
+python3 stitch.py -d ALP_sensitivity/outputs/ALP_60t_300ty
+python3 stitch.py -d ALP_sensitivity/outputs/ALP_60t_600ty
+python3 stitch.py -d ALP_sensitivity/outputs/ALP_60t_1000ty
+
+## HP
+python3 stitch.py -d HP_sensitivity/outputs/HP_60t_100ty
+python3 stitch.py -d HP_sensitivity/outputs/HP_60t_300ty
+python3 stitch.py -d HP_sensitivity/outputs/HP_60t_600ty
+python3 stitch.py -d HP_sensitivity/outputs/HP_60t_1000ty
