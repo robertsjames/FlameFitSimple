9eab41170d8201cd6e4b384882c16c25951eddd6
Rory Matheson <rory_m@login11.chn.perlmutter.nersc.gov>
2025-10-13 03:38:50 -0700

    Updated

--- analyses/WIMP_discovery/run_analysis_benchmark_SURF.sh
+++ analyses/WIMP_discovery/run_analysis_benchmark_SURF.sh
@@ -1,8 +1,8 @@
 
-count=1
+count=0.7
 location=SURF
 
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/2D_Pb214/2D_Pb214_${count}.pkl -o 2D_Pb214_${count}_${location}
-for exposure in 750 800; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/2D_Pb214_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_2D_Pb214/benchmark_${count}_${location}/60t_${exposure}ty
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/2D_neutrons/2D_neutrons_${count}.pkl -o 2D_neutrons_${count}_${location}
+for exposure in 700 750 800 850 900 950; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/2D_neutrons_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_2D_neutrons/benchmark_${count}_${location}/60t_${exposure}ty
 done
