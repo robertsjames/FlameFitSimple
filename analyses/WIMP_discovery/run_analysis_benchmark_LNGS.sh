7d7f55570a041f37bee854211ab064439dd83858
Rory Matheson <rory_m@login39.chn.perlmutter.nersc.gov>
2025-12-01 03:47:20 -0800

    Cleared out old files and cleaned up

--- analyses/WIMP_discovery/run_analysis_benchmark_LNGS.sh
+++ analyses/WIMP_discovery/run_analysis_benchmark_LNGS.sh
@@ -1,7 +1,7 @@
 count=0.1
 location=LNGS
 
-#python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/new_3D_neutrons/3D_neutrons_${count}.pkl -o 3D_neutrons_${count}_${location}
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/new_3D_neutrons/3D_neutrons_${count}.pkl -o 3D_neutrons_${count}_${location}
 for exposure in 50 100 150; do
     python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_3D_neutrons/benchmark_${count}_${location}/60t_${exposure}ty
 done
