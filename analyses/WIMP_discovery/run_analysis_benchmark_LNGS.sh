c41b5b029fa8212682c18723a67f2f270c3a4324
Rory Matheson <rory_m@login10.chn.perlmutter.nersc.gov>
2026-03-11 04:22:03 -0700

    Adding in 'inference_type' as an argument to benchmark_templates.ipynb

--- analyses/WIMP_discovery/run_analysis_benchmark_LNGS.sh
+++ analyses/WIMP_discovery/run_analysis_benchmark_LNGS.sh
@@ -1,7 +1,7 @@
-count=0.1
+count=2.0
 location=LNGS
 
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/3D_neutrons/3D_neutrons_${count}.pkl -o 3D_neutrons_${count}_${location}
-for exposure in 50 100 150; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_3D_neutrons/benchmark_${count}_${location}/60t_${exposure}ty
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/3D_neutrons_revamped/3D_neutron_${count}.pkl -o 3D_neutrons_revamped_${count}_${location}
+for exposure in 550 600 650 700 750; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_revamped_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_3D_neutrons_revamped/benchmark_${count}_${location}/60t_${exposure}ty
 done
--- analyses/WIMP_discovery/run_analysis_benchmark_SURF.sh
+++ analyses/WIMP_discovery/run_analysis_benchmark_SURF.sh
@@ -1,7 +1,7 @@
-count=0.1
+count=5.0
 location=SURF
 
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/new_3D_neutrons/3D_neutrons_${count}.pkl -o 3D_neutrons_${count}_${location}
-for exposure in 50 100 150 200 250; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_3D_neutrons/benchmark_${count}_${location}/60t_${exposure}ty
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/3D_neutrons_revamped/3D_neutron_${count}.pkl -o 3D_neutrons_revamped_${count}_${location}
+for exposure in 800 850 900 950 1000; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_revamped_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_3D_neutrons_revamped/benchmark_${count}_${location}/60t_${exposure}ty
 done
