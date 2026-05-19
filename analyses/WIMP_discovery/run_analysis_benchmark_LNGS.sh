b9c1b2b593e03d62afce611ae1de693b3c88aa0f
Rory Matheson <rory_m@login37.chn.perlmutter.nersc.gov>
2026-05-19 04:19:46 -0700

    saving fogtask

--- analyses/WIMP_discovery/run_analysis_benchmark_LNGS.sh
+++ analyses/WIMP_discovery/run_analysis_benchmark_LNGS.sh
@@ -1,7 +1,7 @@
-count=2.0
+count=1.0
 location=LNGS
 
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/3D_neutrons_revamped/3D_neutron_${count}.pkl -o 3D_neutrons_revamped_${count}_${location}
-for exposure in 550 600 650 700 750; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_revamped_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_3D_neutrons_revamped/benchmark_${count}_${location}/60t_${exposure}ty
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/3D_neutrons_fixed/80t/${count}.pkl -o 3D_neutrons_fixed/80t/${count}_${location}
+for exposure in 550 600 650 700 750 800 850; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_fixed/80t/${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/3D_neutrons_fixed/80t/results/benchmark_${count}_${location}/60t_${exposure}ty
 done
--- analyses/WIMP_discovery/run_analysis_benchmark_SURF.sh
+++ analyses/WIMP_discovery/run_analysis_benchmark_SURF.sh
@@ -1,7 +1,7 @@
-count=5.0
+count=1.0
 location=SURF
 
-python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/3D_neutrons_revamped/3D_neutron_${count}.pkl -o 3D_neutrons_revamped_${count}_${location}
-for exposure in 800 850 900 950 1000; do
-    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons_revamped_${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/WIMP_disco_3D_neutrons_revamped/benchmark_${count}_${location}/60t_${exposure}ty
+python3 ../create_simple_template_likelihood.py -c likelihood_configs/likelihood_benchmark_${location} -t background_templates/3D_neutrons/80t/real/${count}.pkl -o 3D_neutrons/80t/real/5000_toys/${count}_${location}
+for exposure in 750 800 850 900 950 1000 1050 1100; do
+    python3 ../discovery_no_scan/run_routine_discovery_no_scan.py -l likelihoods/3D_neutrons/80t/real/5000_toys/${count}_${location}.pkl -e ${exposure} -c inference_configs/inference_config.ini -o outputs/hedgehog/3D_neutrons/real/80t_components/5000_toys/benchmark_${count}_${location}/60t_${exposure}ty
 done
